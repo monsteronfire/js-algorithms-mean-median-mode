c23ae214ace9b39ad0a4bed08cdeb76256093491
Shaii Ong <monsteronfire@gmail.com>
2017-10-06 23:14:40 +0800

    Add mode function

--- script.js
+++ script.js
@@ -9,7 +9,33 @@ function getMedian(array) {
 }
 
 function getMode(array) {
+  if (array.length === 0) {
+    return null;
+  }
 
+  var modeMap = {};
+  var maxCount = 1;
+  var modes = [array[0]];
+
+  for (var i = 0; i < array.length; i++) {
+    var el = array[i];
+
+    if (modeMap[el] === null) {
+      modeMap[el] = 1;
+    } else {
+      modeMap[el]++;
+    }
+
+    if (modeMap[el] > maxCount) {
+      modes = [el];
+      maxCount = modeMap[el];
+    }
+    else if (modeMap[el] === maxCount) {
+      modes.push(el);
+      maxCount = modeMap[el];
+    }
+  }
+  return modes;
 }
 
 function meanMedianMode(array) {
